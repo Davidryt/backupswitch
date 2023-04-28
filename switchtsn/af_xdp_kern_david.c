bb4758f26bfa72080ad79fb57d3d039f18f4933b
David <myphonedavid@gmail.com>
2023-04-28 12:32:04 +0200

    xdp fix

--- switchtsn/af_xdp_kern_david.c
+++ switchtsn/af_xdp_kern_david.c
@@ -1,10 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/bpf.h>
-//#include <linux/in.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include <linux/if_ether.h> 
+#include <linux/ip.h> 
+#include <linux/udp.h> 
+#include <linux/in.h> 
 #include <stdint.h>
-/*#include <bpf/bpf_endian.h>
 
+/*
 // The parsing helper functions from the packet01 lesson have moved here
 #include "../common/parsing_helpers.h"
 #include "../common/rewrite_helpers.h"
@@ -17,11 +21,21 @@
 #define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
 #endif
 */
+
+#define MAX_SOCKS 64
+
 struct bpf_map_def SEC("maps") xsks_map = {
 	.type = BPF_MAP_TYPE_XSKMAP,
 	.key_size = sizeof(int),
 	.value_size = sizeof(int),
-	.max_entries = 256,  /* Assume netdev has no more than 64 queues */
+	.max_entries = MAX_SOCKS,  
+};
+
+struct bpf_map_def SEC("maps") qidconf_map = {
+	.type        = BPF_MAP_TYPE_ARRAY,
+	.key_size    = sizeof(int),
+	.value_size  = sizeof(int),
+	.max_entries = MAX_SOCKS,
 };
 
 /*struct bpf_map_def SEC("maps") redirect_params = {
@@ -29,15 +43,42 @@ struct bpf_map_def SEC("maps") xsks_map = {
 	.key_size = ETH_ALEN,
 	.value_size = ETH_ALEN,
 	.max_entries = 1,
-};
-*/
-struct bpf_map_def SEC("maps") xdp_stats_map = {
-	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
-	.key_size    = sizeof(int),
-	.value_size  = sizeof(__u32),
-	.max_entries = 64,
-};
+};*/
 
+SEC("xdp,xdp") 
+int xdp_vxlan_redirect(struct xdp_md *ctx) 
+{ 
+	int *qidconf, index = ctx->rx_queue_index; 
+	
+	// A set entry here means that the correspnding queue_id has an active AF_XDP socket bound to it. 
+	qidconf = bpf_map_lookup_elem(&qidconf_map, &index); 
+	if (qidconf) 
+	{ 
+		void *data = (void*)(long)ctx->data; 
+		void *data_end = (void*)(long)ctx->data_end; 
+		
+		struct ethhdr *eth = data;
+		__u16 h_proto = eth->h_proto; 
+		if ((void*)eth + sizeof(*eth) <= data_end) { 
+			if (h_proto == bpf_htons(ETH_P_IP)){ 
+				struct iphdr *ip = data + sizeof(*eth); 
+				if ((void*)ip + sizeof(*ip) <= data_end) { 
+					if (ip->protocol == IPPROTO_UDP){ 
+						struct udphdr *udp = data + sizeof(*eth) + sizeof(*ip); 
+						if ((void*)udp + sizeof(*udp) <= data_end){ 
+							if (udp->dest == bpf_htons(8472)){ 
+								return bpf_redirect_map(&xsks_map, index, 0); 
+				
+							}
+						}
+					}
+				}
+			}
+		}
+	}			
+	return XDP_PASS; 			
+} 
+/*
 SEC("xdp_sock")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
@@ -59,7 +100,7 @@ int xdp_sock_prog(struct xdp_md *ctx)
 
     return XDP_PASS;
 }
-
+*/
 
 /*SEC("xdp_sock")
 int xdp_redirect_func(struct xdp_md *ctx)
